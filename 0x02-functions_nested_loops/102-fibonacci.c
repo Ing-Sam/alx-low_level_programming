1318e5153a5fae11741ef7689b58affbf166a75b
Ing-Sam <mensahsamuel2923@gmail.com>
2023-02-26 13:31:47 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,9 +9,9 @@ int main(void)
 
 {
         int i = 0;
-       long j = 1, k = 2;
-
-        while (i < 50)
+ 	long j = 1, k = 2;
+ 
+	while (i < 50)
         {
                 if (i == 0)
                         printf("%ld", j);
