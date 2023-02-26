b3669214dc2481ae9bfdd6eae49eb7df487fe04b
Ing-Sam <mensahsamuel2923@gmail.com>
2023-02-26 11:04:33 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,12 +19,17 @@ void print_to_98(int n)
 			{
 				printf("%d", n);
 				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d. ", n);
 			}
 		}
 	}
 	else
 	{
-		for (; n>= 98; n--)
+		for (; n >= 98; n--)
 		{
 			if (n == 98)
 			{
